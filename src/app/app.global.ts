b4e4326b0067a047e072a27896bda87e65db57dd
Abhishek Kadam <abhishek.kadam007@gmail.com>
2019-04-19 19:28:58 +0530

    -connect hosted api link.

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -4,12 +4,12 @@ import {BehaviorSubject} from "rxjs";
 @Injectable()
 export class GlobalShared {
   // console.log(window.location.href);
-  // urlpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver.herokuapp.com/uploads/' :
-  //   'http://localhost:8080/uploads/' ;
-  // serverpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver.herokuapp.com/api/' :
-  //   'http://localhost:8080/api/';
-  urlpath: string = 'http://localhost:8080/';
-  serverpath: string = 'https://newtech2.herokuapp.com/api/';
+  urlpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver2.herokuapp.com/' :
+    'http://localhost:8080/' ;
+  serverpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver2.herokuapp.com/graphql/' :
+    'http://localhost:8080/api/';
+  // urlpath: string = 'http://localhost:8080/';
+  // serverpath: string = 'https://newtech2.herokuapp.com/api/';
   defaultimage: string = 'assets/images/default-placeholder.png'; // '../assets/images/default-placeholder.png'
 
   // private productSource = new BehaviorSubject([]);
