e0b44dadd89334a7930020e0833554de0c6c5311
Abhishek Kadam <abhishek.kadam007@gmail.com>
2019-04-20 20:01:57 +0530

    -Change server url.

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -6,10 +6,10 @@ export class GlobalShared {
 
   urlpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver2.herokuapp.com/' :
     'http://localhost:8080/' ;
-  serverpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver2.herokuapp.com/graphql/' :
+ // serverpath: string = ((window.location.href).substr(0,5 ) === 'https') ? 'https://newtechserver2.herokuapp.com/graphql/' :
     'http://localhost:8080/api/';
   // urlpath: string = 'http://localhost:8080/';
-  // serverpath: string = 'https://newtech2.herokuapp.com/api/';
+   serverpath: string = 'https://newtechserver2.herokuapp.com/';
   defaultimage: string = 'assets/images/default-placeholder.png'; // '../assets/images/default-placeholder.png'
 
 }
