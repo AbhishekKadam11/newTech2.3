69501019097f7d41c84d8132b57c054fd2d752ea
Abhishek Kadam <abhishek.kadam007@gmail.com>
2019-04-20 19:52:52 +0530

    -Created server file for hosting.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(sslRedirect());
 
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.use(express.static(__dirname + '/server'));
+// app.use(express.static(__dirname + '/server'));
 app.use(compression()); //compressing dist folder
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
